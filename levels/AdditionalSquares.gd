e861454c758964bd3a4bb881fa8c0cf8a7bbcad2
Leroy Hopson <git@leroy.geek.nz>
2020-11-22 15:02:40 +0700

    Level1 additions

--- levels/AdditionalSquares.gd
+++ levels/AdditionalSquares.gd
@@ -1,18 +1,36 @@
 extends GridMap
 
+
 const START_OFFSET = Vector3(0, -50, 0)
 
+signal tweens_completed()
+
+var rng: RandomNumberGenerator
+var collision_mask_saved: int
+var collision_layer_saved: int
+var tween_count := 0
 
 func _init():
-	visible = true
+	visible = false
+	collision_mask_saved = collision_mask
+	collision_mask = 0
+	collision_layer_saved = collision_layer
+	collision_layer = 0
 
 
 func _ready():
-	return
-	var rng = RandomNumberGenerator.new()
+	rng = RandomNumberGenerator.new()
 	rng.randomize()
-	
+
+
+func _on_tween_completed(_object, _path):
+	tween_count -= 1
+	if tween_count < 5:
+		emit_signal("tweens_completed")
+
+func transition_in():
 	var used_cells := get_used_cells()
+	tween_count = used_cells.size()
 	while not used_cells.empty():
 		yield(get_tree().create_timer(rng.randf_range(0.05, 0.1)), "timeout")
 		var cell = used_cells.pop_back()
@@ -31,9 +49,16 @@ func _ready():
 					rng.randf_range(-5, 5),
 					rng.randf_range(-5, 5),
 					rng.randf_range(-5, 5)
-				), cell, rng.randf_range(1, 3),
+				), cell, rng.randf_range(0.5, 1),
 				Tween.TRANS_ELASTIC, Tween.EASE_OUT)
 		mesh_instance.add_child(tween)
 		mesh_instance.translation = cell + START_OFFSET
 		get_parent().add_child(mesh_instance)
+		tween.connect("tween_completed", self, "_on_tween_completed")
 		tween.start()
+	yield(self, "tweens_completed")
+	visible = true
+	collision_layer = collision_layer_saved
+	collision_mask = collision_mask_saved
+	for child in get_children():
+		child.free()
--- levels/level_1.gd
+++ levels/level_1.gd
@@ -1,5 +1,22 @@
 extends Spatial
 
 
+signal continue_level()
+
+
 func _ready():
-	$Player/Console.message("Use h j k l to move")
+	$Part2.transition_in()
+	yield(get_tree().create_timer(3), "timeout")
+	$Player/Console.message("Use h j k l to move\r\n\nPress ENTER to continue")
+	yield(self, "continue_level")
+	$Part3.transition_in()
+	$Player/MovementHints.visible = true
+	#yield($Part3Trigger, "body_entered")
+	#$Part3.transition_in()
+
+
+
+func _input(event):
+	if event.is_action_pressed("continue"):
+		print("TODO continue")
+		emit_signal("continue_level")
