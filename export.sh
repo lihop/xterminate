8099cdda1f77c22a4165dab245c16552dd8b85a8
Leroy Hopson <git@leroy.geek.nz>
2020-11-24 12:45:38 +0700

    Export working for MacOS
    Python modules still need to be installed on MacOS by going to the
    project directory and:
    	$ cd Xterminate.app/Contents/Resources
    	$ ./addons/pythonscript/osx-64/bin/python3 -m ensurepip
    	$ ./addons/pythonscript/osx-64/bin/python3 -m pip install -r requirements.txt

--- export.sh
+++ export.sh
@@ -9,6 +9,28 @@ fixup_nixos() {
 	echo ELF patched!
 }
 
+fixup_osx() {
+	echo Fixing up for osx...
+	unzip xterminate.zip
+	mv pack.zip Xterminate.app/Contents/Resources/
+	cd Xterminate.app/Contents/Resources
+	rm Xterminate.pck
+	unzip pack.zip
+	rm pack.zip
+	mkdir addons/pythonscript
+	cp -L -r ../../../../../addons/pythonscript/osx-64 addons/pythonscript/
+	chmod +x addons/pythonscript/osx-64/bin/python3
+	cd ../MacOS
+	mv ./Xterminate ../Resources
+	echo '#!/bin/bash' > Xterminate
+	echo 'DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"' >> Xterminate
+	echo 'cd $DIR/../Resources' >> Xterminate
+	echo './Xterminate' >> Xterminate
+	chmod +x Xterminate
+	cd ../../../
+	rm xterminate.zip
+}
+
 generate_export() {
 	target=$1
 	echo "Exporting target $target"
@@ -70,6 +92,7 @@ generate_export() {
 	fi
 
 	cd ..
+	rm xterminate.${target}.zip || true
 	zip xterminate.${target}.zip -r xterminate.${target}
 	cd ..
 	rm -rf $tmpdir
